ce19df97903591132ff2477e70e33d71070a084f
Elyor Xatanboyev <elxatamboyev2005@gmail.com>
2025-07-20 08:25:10 +0500

    .

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿
 using Lesson26;
+using Lesson26.BotMessage;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Telegram.Bot;
@@ -7,13 +8,13 @@ using Telegram.Bot.Polling;
 
 var builder = Host.CreateApplicationBuilder();
 
-builder.Services.AddSingleton<IUpdateHandler, BotUpdateHandler>();
-
 builder.Services.AddSingleton<ITelegramBotClient, TelegramBotClient>( x =>
     new TelegramBotClient(builder.Configuration["Bot:Token"]
          ?? throw new ArgumentException("Telegram bot token is not configured")));
 
 builder.Services.AddHostedService<BotHostedService>();
+builder.Services.AddSingleton<IUpdateHandler, BotUpdateHandler>();
+builder.Services.AddSingleton<IBotMessage, BotMessage>();
 
 var app = builder.Build();
 
