19827f91b32d1e8250ff6a8dd522004ce318bece
Elyor Xatanboyev <elxatamboyev2005@gmail.com>
2025-07-24 18:51:27 +0500

    .

--- obj/Debug/net8.0/lesson26.AssemblyInfo.cs
+++ obj/Debug/net8.0/lesson26.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lesson26")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f305ca1f662df64a763148221ec382eca7dd739f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28b5a893c3f2e1d6aef99aba63bb341787927b40")]
 [assembly: System.Reflection.AssemblyProductAttribute("lesson26")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lesson26")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
